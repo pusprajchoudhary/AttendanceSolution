7fecaff28406039035f55ac9501c37a2fe8681be
pusprajchoudhary <pusprajchoudhary12345@gmail.com>
2025-05-22 00:29:29 +0530

    Fixed leave model imports and standardized route file naming

--- backend/routes/leaveRoutes.js
+++ backend/routes/leaveRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Leave = require('../models/leaveModel');
+const Leave = require('../models/LeaveModel');
 const { protect, admin } = require('../middleware/authMiddleware');
 
 // Apply for leave
